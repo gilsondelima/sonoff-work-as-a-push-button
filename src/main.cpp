b8349429e796ab5a6e303fa6bc368fc133c8b44d
samiulbasher <77229506+samiulbasher@users.noreply.github.com>
2021-03-05 23:52:15 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,13 @@
 #include <SinricPro.h>
 #include <SinricProSwitch.h>
 
+/*--------------You need to fill this out. Otherwise, the program will not work.------------------------*/
 #define WIFI_SSID         "YOUR-WIFI-SSID"    // your own wifi network 
 #define WIFI_PASS         "YOUR-WIFI-PASS"    // wifi network password
 #define APP_KEY           "YOUR-APP-KEY"      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
 #define APP_SECRET        "YOUR-APP-SECRET"   // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
 #define SWITCH_ID         "YOUR-DEVICE-ID"    // Should look like "5dc1564130xxxxxxxxxxxxxx"
-
+/*-----------------------------------------------------------------------------------------------------*/
 
 #define BAUD_RATE          9600       
 
